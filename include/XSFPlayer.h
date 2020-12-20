bbed2d52861d6e14679a0dc2d750aaee2d95b6e2
Restyled.io <commits@restyled.io>
2020-12-20 09:10:14 +0000

    Restyled by clang-format

--- include/XSFPlayer.h
+++ include/XSFPlayer.h
@@ -20,59 +20,51 @@
 // this.
 class XSFPlayer {
 protected:
-    static const uint32_t CHECK_SILENCE_BIAS = 0x8000000;
-    static const uint32_t CHECK_SILENCE_LEVEL = 7;
+  static const uint32_t CHECK_SILENCE_BIAS = 0x8000000;
+  static const uint32_t CHECK_SILENCE_LEVEL = 7;
 
-    std::unique_ptr<XSFFile> xSF;
-    unsigned sampleRate, detectedSilenceSample, detectedSilenceSec,
-             skipSilenceOnStartSec, lengthSample, fadeSample, currentSample;
-    uint32_t prevSampleL, prevSampleR;
-    int lengthInMS, fadeInMS;
-    double volume;
-    bool ignoreVolume, uses32BitSamplesClampedTo16Bit;
+  std::unique_ptr<XSFFile> xSF;
+  unsigned sampleRate, detectedSilenceSample, detectedSilenceSec,
+      skipSilenceOnStartSec, lengthSample, fadeSample, currentSample;
+  uint32_t prevSampleL, prevSampleR;
+  int lengthInMS, fadeInMS;
+  double volume;
+  bool ignoreVolume, uses32BitSamplesClampedTo16Bit;
 
-    XSFPlayer();
-    XSFPlayer(const XSFPlayer &xSFPLayer);
+  XSFPlayer();
+  XSFPlayer(const XSFPlayer &xSFPLayer);
 
 public:
-    // These are not defined in XSFPlayer.cpp, they should be defined in your own
-    // player's source.  The Create functions should return a pointer to your
-    // player's class.
-    static const char *WinampDescription;
-    static const char *WinampExts;
-    static XSFPlayer *Create(const std::string &fn);
+  // These are not defined in XSFPlayer.cpp, they should be defined in your own
+  // player's source.  The Create functions should return a pointer to your
+  // player's class.
+  static const char *WinampDescription;
+  static const char *WinampExts;
+  static XSFPlayer *Create(const std::string &fn);
 #ifdef _WIN32
-    static XSFPlayer *Create(const std::wstring &fn);
+  static XSFPlayer *Create(const std::wstring &fn);
 #endif
 
-    virtual ~XSFPlayer() {}
-    XSFPlayer &operator=(const XSFPlayer &xSFPlayer);
-    XSFFile *GetXSFFile() {
-        return this->xSF.get();
-    }
-    const XSFFile *GetXSFFile() const {
-        return this->xSF.get();
-    }
-    unsigned GetLengthInSamples() const {
-        return this->lengthSample + this->fadeSample;
-    }
-    unsigned GetSampleRate() const {
-        return this->sampleRate;
-    }
-    void SetSampleRate(unsigned newSampleRate) {
-        this->sampleRate = newSampleRate;
-    }
-    void IgnoreVolume() {
-        this->ignoreVolume = true;
-    }
-    virtual bool Load();
-    bool FillBuffer(std::vector<uint8_t> &buf, unsigned &samplesWritten);
-    virtual void GenerateSamples(std::vector<uint8_t> &buf, unsigned offset,
-                                 unsigned samples) = 0;
-    void SeekTop();
+  virtual ~XSFPlayer() {}
+  XSFPlayer &operator=(const XSFPlayer &xSFPlayer);
+  XSFFile *GetXSFFile() { return this->xSF.get(); }
+  const XSFFile *GetXSFFile() const { return this->xSF.get(); }
+  unsigned GetLengthInSamples() const {
+    return this->lengthSample + this->fadeSample;
+  }
+  unsigned GetSampleRate() const { return this->sampleRate; }
+  void SetSampleRate(unsigned newSampleRate) {
+    this->sampleRate = newSampleRate;
+  }
+  void IgnoreVolume() { this->ignoreVolume = true; }
+  virtual bool Load();
+  bool FillBuffer(std::vector<uint8_t> &buf, unsigned &samplesWritten);
+  virtual void GenerateSamples(std::vector<uint8_t> &buf, unsigned offset,
+                               unsigned samples) = 0;
+  void SeekTop();
 #ifdef WINAMP_PLUGIN
-    int Seek(unsigned seekPosition, volatile int *killswitch,
-             std::vector<uint8_t> &buf, Out_Module *outMod);
+  int Seek(unsigned seekPosition, volatile int *killswitch,
+           std::vector<uint8_t> &buf, Out_Module *outMod);
 #endif
-    virtual void Terminate() = 0;
+  virtual void Terminate() = 0;
 };
--- include/eqstr.h
+++ include/eqstr.h
@@ -16,25 +16,25 @@
 #include <string>
 
 struct eq_str : std::binary_function<std::string, std::string, bool> {
-    struct eq_char : std::binary_function<char, char, bool> {
-        const char *tab;
-        eq_char(const char *t) : tab(t) {}
-        bool operator()(char x, char y) const {
-            return this->tab[x - CHAR_MIN] == this->tab[y - CHAR_MIN];
-        }
-    };
-
-    char tab[CHAR_MAX - CHAR_MIN + 1];
-
-    eq_str(const std::locale &L = std::locale::classic()) {
-        for (int i = CHAR_MIN; i <= CHAR_MAX; ++i)
-            this->tab[i - CHAR_MIN] = static_cast<char>(i);
-        std::use_facet<std::ctype<char>>(L).toupper(
-                                          this->tab, this->tab + (CHAR_MAX - CHAR_MIN + 1));
+  struct eq_char : std::binary_function<char, char, bool> {
+    const char *tab;
+    eq_char(const char *t) : tab(t) {}
+    bool operator()(char x, char y) const {
+      return this->tab[x - CHAR_MIN] == this->tab[y - CHAR_MIN];
     }
+  };
 
-    bool operator()(const std::string &x, const std::string &y) const {
-        return x.length() == y.length() &&
-               std::equal(x.begin(), x.end(), y.begin(), eq_char(this->tab));
-    }
+  char tab[CHAR_MAX - CHAR_MIN + 1];
+
+  eq_str(const std::locale &L = std::locale::classic()) {
+    for (int i = CHAR_MIN; i <= CHAR_MAX; ++i)
+      this->tab[i - CHAR_MIN] = static_cast<char>(i);
+    std::use_facet<std::ctype<char>>(L).toupper(
+        this->tab, this->tab + (CHAR_MAX - CHAR_MIN + 1));
+  }
+
+  bool operator()(const std::string &x, const std::string &y) const {
+    return x.length() == y.length() &&
+           std::equal(x.begin(), x.end(), y.begin(), eq_char(this->tab));
+  }
 };
