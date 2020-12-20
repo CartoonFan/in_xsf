7b82dcd850ab4a4df1fbb405770d35624787754f
Restyled.io <commits@restyled.io>
2020-12-20 08:31:22 +0000

    Restyled by astyle

--- include/XSFPlayer_NCSF.h
+++ include/XSFPlayer_NCSF.h
@@ -19,30 +19,30 @@
 
 class XSFPlayer_NCSF : public XSFPlayer
 {
-	uint32_t sseq;
-	std::vector<uint8_t> sdatData;
-	std::unique_ptr<SDAT> sdat;
-	Player player;
-	double secondsPerSample, secondsIntoPlayback, secondsUntilNextClock;
-	std::bitset<16> mutes;
+    uint32_t sseq;
+    std::vector<uint8_t> sdatData;
+    std::unique_ptr<SDAT> sdat;
+    Player player;
+    double secondsPerSample, secondsIntoPlayback, secondsUntilNextClock;
+    std::bitset<16> mutes;
 
-	void MapNCSFSection(const std::vector<uint8_t> &section);
-	bool MapNCSF(XSFFile *xSFToLoad);
-	bool RecursiveLoadNCSF(XSFFile *xSFToLoad, int level);
-	bool LoadNCSF();
+    void MapNCSFSection(const std::vector<uint8_t> &section);
+    bool MapNCSF(XSFFile *xSFToLoad);
+    bool RecursiveLoadNCSF(XSFFile *xSFToLoad, int level);
+    bool LoadNCSF();
 public:
-	XSFPlayer_NCSF(const std::string &filename);
+    XSFPlayer_NCSF(const std::string &filename);
 #ifdef _WIN32
-	XSFPlayer_NCSF(const std::wstring &filename);
+    XSFPlayer_NCSF(const std::wstring &filename);
 #endif
-	~XSFPlayer_NCSF();
-	bool Load();
-	void GenerateSamples(std::vector<uint8_t> &buf, unsigned offset, unsigned samples);
-	void Terminate();
+    ~XSFPlayer_NCSF();
+    bool Load();
+    void GenerateSamples(std::vector<uint8_t> &buf, unsigned offset, unsigned samples);
+    void Terminate();
 
-	void SetInterpolation(unsigned interpolation);
-	void SetMutes(const std::bitset<16> &newMutes);
+    void SetInterpolation(unsigned interpolation);
+    void SetMutes(const std::bitset<16> &newMutes);
 #ifdef _DEBUG
-	const Channel &GetChannel(size_t chanNum) const;
+    const Channel &GetChannel(size_t chanNum) const;
 #endif
 };
